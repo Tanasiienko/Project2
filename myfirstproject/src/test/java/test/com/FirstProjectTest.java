0de08f72a92b3cd82eb437d0df103b423e01050d
Tanasiienko <153381381+Tanasiienko@users.noreply.github.com>
2024-01-20 00:06:00 -0500

    This is First

---
+++ myfirstproject/src/test/java/test/com/FirstProjectTest.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com;
+
+import com.itexps.training.myfirstproject.MyFirst;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author pahan
+ */
+public class FirstProjectTest {
+    MyFirst stu;
+    public FirstProjectTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    
+    @Test
+    public void testName () {
+        //Instantiate it here instead of beforemethod
+        stu = new MyFirst (1, "Mike"); // This is what I want to test (my test data) 
+        assertEquals (stu.getName(), "Mike");   // (actual, expected)
+    }
+    
+    
+}
